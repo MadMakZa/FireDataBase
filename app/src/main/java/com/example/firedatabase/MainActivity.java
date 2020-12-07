bcc32bef715c170774088e4f1b5ac9cd23939d7c
MadMakZa <immakza@gmail.com>
2020-12-07 22:38:47 +0300

    add logic button Save

--- app/src/main/java/com/example/firedatabase/MainActivity.java
+++ app/src/main/java/com/example/firedatabase/MainActivity.java
@@ -6,9 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText edName, edSecondName, edEmail;
+    private DatabaseReference mDataBase;
+    private String USER_KEY = "User";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +26,20 @@ public class MainActivity extends AppCompatActivity {
         edName = findViewById(R.id.edName);
         edSecondName = findViewById(R.id.edSecondName);
         edEmail = findViewById(R.id.edEmail);
+        //создание базы данных
+        mDataBase = FirebaseDatabase.getInstance().getReference(USER_KEY);
     }
     //кнопочки
     public void onClickSave(View view){
-
+        String id = mDataBase.getKey(); //получаем ключ с базы данных
+        //записываем введенный текст в переменные
+        String name = edName.getText().toString();
+        String secondName = edSecondName.getText().toString();
+        String email = edEmail.getText().toString();
+        //создание нового пользователя с введенными выше параметрами
+        User newUser = new User(id,name,secondName,email);
+        //вносим новго пользователя в базу данных
+        mDataBase.push().setValue(newUser);
     }
     public void onClickRead(View view){
 
