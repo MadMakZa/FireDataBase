6c8eafc4c869a7c53385c5ee95f5ee15bbed5852
MadMakZa <immakza@gmail.com>
2020-12-07 22:28:21 +0300

    create class User

--- app/src/main/java/com/example/firedatabase/MainActivity.java
+++ app/src/main/java/com/example/firedatabase/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.firedatabase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edName, edSecondName, edEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    public void init(){
+        edName = findViewById(R.id.edName);
+        edSecondName = findViewById(R.id.edSecondName);
+        edEmail = findViewById(R.id.edEmail);
+    }
+    //кнопочки
+    public void onClickSave(View view){
+
+    }
+    public void onClickRead(View view){
+
     }
 }
---
+++ app/src/main/java/com/example/firedatabase/User.java
@@ -0,0 +1,17 @@
+package com.example.firedatabase;
+
+public class User {
+    public String id, name, sec_name, email;
+
+    //пустой конструктор
+    public User(){
+
+    }
+    //не пустой конструктор
+    public User(String id, String name, String sec_name, String email) {
+        this.id = id;
+        this.name = name;
+        this.sec_name = sec_name;
+        this.email = email;
+    }
+}
