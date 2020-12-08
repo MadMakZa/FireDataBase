34cb4dc97574ad017cb8356e42ff15bb2f84565e
MadMakZa <immakza@gmail.com>
2020-12-08 19:48:39 +0300

    add class constants

--- app/src/main/java/com/example/firedatabase/ShowActivity.java
+++ app/src/main/java/com/example/firedatabase/ShowActivity.java
@@ -17,6 +17,7 @@ public class ShowActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.show_layout);
         init();
+        getIntentMain();
     }
 
     private void init(){
