8b49adaf67cfee0170d999b8aa5eb533f2e61422
MadMakZa <immakza@gmail.com>
2020-12-11 22:16:26 +0300

    add image view on show activity

--- app/src/main/java/com/example/firedatabase/ShowActivity.java
+++ app/src/main/java/com/example/firedatabase/ShowActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,6 +13,8 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ShowActivity extends AppCompatActivity {
 
     private TextView tvName, tvSecondName, tvEmail;
+    private ImageView imBD;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +27,7 @@ public class ShowActivity extends AppCompatActivity {
         tvName = findViewById(R.id.tvName);
         tvSecondName = findViewById(R.id.tvSecondName);
         tvEmail = findViewById(R.id.tvEmail);
+        imBD = findViewById(R.id.imBD);
     }
 
     private void getIntentMain(){
